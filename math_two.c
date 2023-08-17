b6aaf9983bbe1059f314f970d2f459fe6c8df659
asif-kamal <asif.kamal1489@gmail.com>
2023-08-17 18:47:22 -0400

    pow function from the math.h lib

--- math_two.c
+++ math_two.c
@@ -4,10 +4,15 @@
 int main() {
 
     float r; 
+    float p;
+
+    p = pow(2.0, 8.0);
 
     r = sqrt(2.0);
 
     printf("The square root of 2 is %f\n", r);
+
+    printf("2 to the 8th power is %.2f\n", p);
 
     return(0);
 }
